8502caff32abcb7ca1bd6e8a84aae5e596eecd00
Stephen Carboni <stephen.entropy@gmail.com>
2016-08-09 13:53:54 +0200

    add new user complete

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import request
+from flask import make_response
+from flask import Flask
+
+import mysql.connector
+
+import bcrypt
+
+import config
+from util import encode_json
+
+DEBUG = True
+
+app = Flask(__name__)
+
+
+def make_success_response(data, code=200):
+    payload = encode_json({'success':  True, 'data': data})
+    resp = make_response(payload, code)
+    return resp
+
+
+def make_failed_response(error_message, code=400):
+    payload = encode_json({'success': False, 'error': error_message})
+    resp = make_response(payload, code)
+    return resp
+
+
+@app.route("/user", methods=['POST'])
+def user():
+    
+    # add a new user
+    if request.method == "POST": 
+    
+        cnx = mysql.connector.connect(**config.db)        
+        cursor = cnx.cursor(named_tuple=True)
+        
+        new_user = request.get_json()
+        
+        hashed_password = bcrypt.hashpw(new_user['password'].encode(), bcrypt.gensalt())
+        
+        try:
+            print('User INSERT')
+            cursor.execute(
+                """ INSERT INTO user (email, fname, lname, type, password)
+                               VALUES (%s, %s, %s, %s, %s); """,
+                (
+                    new_user["email"],
+                    new_user["fname"],
+                    new_user["lname"],
+                    new_user["type"],
+                    hashed_password
+                )
+            )
+                            
+        except Exception as e:
+            cnx.rollback()
+            return make_failed_response(str(e))
+        else:
+            cnx.commit()
+            data = dict(id=cursor.lastrowid)
+            return make_success_response(data)
+        finally:
+            cursor.close()
+            cnx.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=DEBUG)
